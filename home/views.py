42e5012d4d87019afaae1c36a2ac2ddd6c83e468
Sheldon <sheldonchettiar3@gmail.com>
2023-02-24 01:46:09 +0530

    added static and templates directory

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("this is homepage")
+    return render(request, 'index.html')
+    # return HttpResponse("this is homepage")
 def about(request):
     return HttpResponse("this is about")
